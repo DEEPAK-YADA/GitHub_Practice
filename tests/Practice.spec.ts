9aa5cc29bf512b18e5b184f441261c68e0b28555
Deepak Yadav <92750936+DEEPAK-YADA@users.noreply.github.com>
2025-11-14 10:40:46 +0530

    Update Practice.spec.ts

--- tests/Practice.spec.ts
+++ tests/Practice.spec.ts
@@ -1,5 +1,5 @@
 import { test, expect } from '@playwright/test';
-
+//this is my first test.
 test('has title', async ({ page }) => {
   await page.goto('https://demo.automationtesting.in/Register.html');
 
