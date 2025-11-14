e339ee9429723c7b8dec3b4d1e98b1aeb9e36539
DEEPAK-YADA <deepaksel28@gmail.com>
2025-11-14 10:43:24 +0530

    update testcases name

--- tests/Practice.spec.ts
+++ tests/Practice.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
 //this is my first test.
-test('has title', async ({ page }) => {
+test('Test automation title', async ({ page }) => {
   await page.goto('https://demo.automationtesting.in/Register.html');
 
   // Expect a title "to contain" a substring.
